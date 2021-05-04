45190c32d0c2912bdfd231cc7b3ec5c6996c27f7
Dan Tuls <spimonid@gmail.com>
2021-05-03 21:26:50 -0500

    added contact in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create(first_name: "Dan", last_name: "Tuls", email: "dt@test.com", phone_number: "231-225-1949")
+Contact.create(first_name: "Tina", last_name: "Smith", email: "christina@test.com", phone_number: "231-335-2819")
+Contact.create(first_name: "Phil", last_name: "Jones", email: "philip@test.com", phone_number: "231-987-3711")
+Contact.create(first_name: "Jane", last_name: "Smithers", email: "janeellen@test.com", phone_number: "231-229-3128")
